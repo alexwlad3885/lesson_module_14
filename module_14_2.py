daa86269241d9d7c5230cbfcfef8b0871d02e7f9
alexwlad3885 <alexwlad3885@yandex.ru>
2024-09-11 21:12:59 +0300

    Домашнее задание по теме 'Выбор элементов и функции в SQL запросах'

---
+++ module_14_2.py
@@ -0,0 +1,44 @@
+# - * - coding: utf - 8 - * -
+"""Домашнее задание по теме 'Выбор элементов и функции в SQL запросах'"""
+import sqlite3
+
+connection = sqlite3.connect("not_telegram.db")
+cursor = connection.cursor()
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS Users(
+id INTEGER PRIMARY KEY,
+username TEXT NOT NULL,
+email TEXT NOT NULL,
+age INTEGER,
+balance INTEGER NOT NULL
+)
+''')
+cursor.execute("CREATE INDEX IF NOT EXISTS idx_email ON Users(email)")
+
+for i in range(1, 11):
+    cursor.execute("INSERT INTO Users (username, email, age, balance) VALUES (?, ?, ?, ?)",
+                   (f'User{i}', f'example{i}@gmail.com', f'{i * 10}', 1000))
+
+for i in range(1, 11, 2):
+    cursor.execute("UPDATE Users SET balance = ? WHERE username = ?", (500, f'User{i}'))
+
+for i in range(1, 11, 3):
+    cursor.execute("DELETE FROM Users WHERE username = ?", (f'User{i}',))
+
+cursor.execute("SELECT username, email, age, balance FROM Users WHERE age != ?", (60,))
+users = cursor.fetchall()
+for user in users:
+    print(f'Имя: {user[0]} | Почта: {user[1]} | Возраст: {user[2]} | Баланс: {user[3]}')
+
+# Удаление пользователя с id=6
+cursor.execute("DELETE FROM Users WHERE id = ?", (6,))
+# Подсчёт кол-ва всех пользователей
+cursor.execute("SELECT COUNT (*) FROM Users")
+total_users = cursor.fetchone()[0]
+# Подсчёт суммы всех балансов
+cursor.execute("SELECT SUM(balance) FROM Users")
+all_balances = cursor.fetchone()[0]
+print(f'{all_balances / total_users}')
+
+connection.commit()
+connection.close()
