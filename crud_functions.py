857f12d3816c0d89467cce78b9517c24ef6785cb
alexwlad3885 <alexwlad3885@yandex.ru>
2024-09-14 00:03:02 +0300

    Домашнее задание по теме 'Написание примитивной ORM'

--- crud_functions.py
+++ crud_functions.py
@@ -12,6 +12,17 @@ def initiate_db():
     price INTEGER NOT NULL
     )
     ''')
+    connection.commit()
+    cursor.execute('''
+    CREATE TABLE IF NOT EXISTS Users(
+    id INTEGER PRIMARY KEY,
+    username TEXT NOT NULL,
+    email TEXT NOT NULL,
+    age INTEGER NOT NULL,
+    balance INTEGER NOT NULL
+    )
+    ''')
+    connection.commit()
 
 
 def get_all_products():
@@ -20,3 +31,21 @@ def get_all_products():
     cursor.execute("SELECT * FROM Products")
     all_products = cursor.fetchall()
     return all_products
+
+
+def add_user(username, email, age):
+    connection = sqlite3.connect("alb_products.db")
+    cursor = connection.cursor()
+    cursor.execute("INSERT INTO Users(username, email, age, balance) VALUES (?, ?, ?, ?)",
+                   (f'{username}', f'{email}', f'{age}', 1000))
+    connection.commit()
+
+
+def is_included(username):
+    connection = sqlite3.connect("alb_products.db")
+    cursor = connection.cursor()
+    check_user = cursor.execute("SELECT * FROM Users WHERE username = ?", (username,))
+    if check_user.fetchone() is None:
+        return False
+    else:
+        return True
